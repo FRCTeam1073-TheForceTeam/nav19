3c6821b9771d860abbf7d60d0bf61889a0a83913
claudp <claudiapack@charter.net>
2019-02-01 23:00:39 -0500

    First pass serving HTTP image (JPG) using Python Imaging Library and http.server class.

--- vision/openmv/video.py
+++ vision/openmv/video.py
@@ -5,17 +5,28 @@
 import sensor, image, time
 import pyb
 
+fmt = sensor.RGB565
+res = sensor.QVGA
 led1 = pyb.LED(1)
 led2 = pyb.LED(2)
 
+file = open("camId.txt")
+cam = int(file.readline())
+file.close()
 sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565
-sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
+sensor.set_pixformat(fmt) # Set pixel format to RGB565
+sensor.set_framesize(res)  # Set frame size to QVGA (320x240)
 led1.on()
 sensor.skip_frames(time = 1500)     # Wait for settings take effect.
 led1.off()
 clock = time.clock()                # Create a clock object to track the FPS.
+startOfPacket = { "cam": cam, "time": pyb.elapsed_millis(0), "fmt": fmt, "height": sensor.height(), "width": sensor.width()}
+endOfPacket = { "end": 0}
+
 
 while(True):
     clock.tick()                    # Update the FPS clock.
+    startOfPacket["time"] = pyb.elapsed_millis(0)
+    print(startOfPacket)
     img = sensor.snapshot()         # Take a picture and return the image.
+    print(endOfPacket)
--- vision/visionmanager.py
+++ vision/visionmanager.py
@@ -1,6 +1,13 @@
 from cameramanager import CameraManager			#see cameramanager.py
 from networktables import NetworkTables
 import time
+import sys
+import io
+import http.server
+import threading
+from PIL import Image
+
+
 
 def give_script(mode):
         if mode == "lines":
@@ -16,25 +23,58 @@ def set_mode(cam, mode):
                 script = fin.read()
         print(script)
         cam.stop_script()
+        cam.enable_fb(True)
         cam.exec_script(script)
 
+#WARNING!!!! Global variable
+imgData = io.BytesIO()
 
+class ImageHandler(http.server.BaseHTTPRequestHandler):
+        
+        def do_GET(self):
+                global imgData 
+                # Send response status code
+                self.send_response(200)
+ 
+                # Send headers
+                self.send_header('Content-type','image/jpeg')
+                self.end_headers()
+
+                #print("Sending Image.%d" % imgData.tell())
+                # Write content as utf-8 data
+                self.wfile.write(imgData.getvalue())
+                return
+        
+        # This stops it spewing output all the time.
+        def log_message(self, format, *args):
+                return
+
+        
 #camera initialization
-cam0mode = "lines"
+cam0mode = sys.argv[1]
 cam0 = CameraManager("/dev/ttyACM0")
 
 #networkTables initialization
 NetworkTables.initialize()
 nt = NetworkTables.getTable("CameraFeedback")
+nt.putString("cam_0_mode", cam0mode)
 
+
+# create image webserver
+
+server_address = ('127.0.0.1', 8081)
+httpd = http.server.HTTPServer(server_address, ImageHandler)
+httpdThread = threading.Thread(target = httpd.serve_forever)
+httpdThread.start()
+                                 
 #main loop
 cam0frame = 0
 
 set_mode(cam0, cam0mode)
 while True:
         cam0.processData()
+        cam0frame = cam0frame + 1
         if len(cam0.data) > 0:
-                cam0frame = cam0frame + 1
                 if cam0mode == "lines":
                         data = []
                         for line in cam0.data:
@@ -59,17 +99,25 @@ while True:
 
                 elif cam0mode == "video":
                         data = []
-                        nt.putNumberArray("cam_0_blobs", data)
+                        nt.putNumberArray("cam_0_blobs", [])
                         nt.putNumberArray("cam_0_lineseg", [])
                         
         nt.putString("cam_0_status", "ok")
         nt.putNumber("cam_0_frame", cam0frame)
-        newmode = nt.getString("cam_0_mode", cam0mode)
         nt.putNumber("cam_0_width", cam0.width)
         nt.putNumber("cam_0_height", cam0.height)
+        
+        newmode = nt.getString("cam_0_mode", cam0mode)
         if newmode != cam0mode:
                 cam0mode = newmode
                 set_mode(cam0, cam0mode)
-
-
                 
+        fb = cam0.fb_dump()
+        if fb != None:
+                image = Image.fromarray(fb[2])
+                imgData.seek(0)
+                image.save(imgData, format = "JPEG")
+                imgData.truncate()
+
+
+
