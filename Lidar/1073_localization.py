4bdfac0f4ac013b3c3a6f29142b96c780fd9ad17
kcjesse02 <kcjesse02@gmail.com>
2019-02-24 14:22:31 -0800

    complete extra-simplified 1073_localization
    gets scans and prints it out.

--- Lidar/1073_localization.py
+++ Lidar/1073_localization.py
@@ -4,30 +4,27 @@
 author: 
 
 """
-LIDAR_DEVICE = '/dev/cu.SLAB_USBtoUART' #Cam - where is LiDAR, change to COM5 on most Windows Machines, "/dev/ttyUSB0" on Raspberry Pi, Mac, and Ubuntu
+LIDAR_DEVICE = 'COM3' #'/dev/cu.SLAB_USBtoUART' #Cam - where is LiDAR, change to COM5 on most Windows Machines, "/dev/ttyUSB0" on Raspberry Pi, Mac, and Ubuntu
 import sys
 import time
-import networktables
-from networktables import NetworkTables
-from multiprocessing import Process
+#import networktables
+#from networktables import NetworkTables
 import numpy as np
-import math
-import matplotlib.pyplot as plt
 from rplidar import RPLidar as Lidar #Cam - import RPLidar
 import fieldScanner #Katherine - import the scanner methods
-Table = networktables.NetworkTablesInstance()
-LidarTable = Table.getTable("1073Table")
+#Table = networktables.NetworkTablesInstance()
+#LidarTable = Table.getTable("1073Table")
 lidarArray = []
 
 
 def Odometry(array):
-    accelx= LidarTable.getNumber("accelx", 0)
+    accelx= 0#LidarTable.getNumber("accelx", 0)
     array.append(accelx)
-    accely = LidarTable.getNumber("accely", 0)
+    accely = 0#LidarTable.getNumber("accely", 0)
     array.append(accely)
-    accelz = LidarTable.getNumber("accelz")
+    accelz = 0#LidarTable.getNumber("accelz")
     array.append(accelz)
-    gyro = LidarTable.getNumber("gyroRawValue", 0)
+    gyro = 0#LidarTable.getNumber("gyroRawValue", 0)
     print("Odometry complete!")
     return array
 #def LidarArray(array):
@@ -37,33 +34,28 @@ def main(path):
         print(__file__ + " start!!")
 
         lidar = Lidar(LIDAR_DEVICE)
+        time.sleep(3)
         data = []
         try:
+          
             print('Recording measurments... Press Crl+C to stop.')
             for scan in lidar.iter_scans():
-                data.append(np.array(scan))
+                #TO DO - call feature extract
 
+                #TO DO - call localizer
+
+                #TO DO - publish to network table
+
+                data.append(np.array(scan))
+                print(scan)
         except KeyboardInterrupt:
             print('Stoping.')
 
         lidar.stop()
+        lidar.stop_motor()
         lidar.disconnect()
-        np.save(path, np.array(data))
-
-        #lidar.start_motor()
-        #for scans in lidar.iter_scans():
-
-            # array.append[(degrees, distance)]
-
-            #TO DO - call feature extract
-
-            #TO DO - call localizer
-
-            #TO DO - publish to network table
-
-            #print("scan number: " + len(scans))
-    
-       # return
+        
+        return
 
 if __name__ == '__main__':
     main(sys.argv[0])
