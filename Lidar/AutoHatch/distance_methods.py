e5be26c84d73ee9fc8f2ba39265334385f09f356
Ben Wertz <benwertz216@gmail.com>
2019-03-26 19:19:52 -0400

    Update distance_methods.py

--- Lidar/AutoHatch/distance_methods.py
+++ Lidar/AutoHatch/distance_methods.py
@@ -33,7 +33,7 @@ class auto_hatch:
         for i in range(len(degreesAndDistanceArray)-1):
             point1 = degreesAndDistanceArray[i]
             point2 = degreesAndDistanceArray[i+1]
-            degreesBetween = point2[0] - point[0]
+            degreesBetween = point2[0] - point1[0]
             #can we have the lidar mounted so the first 180 degrees are facing outward?
 
             distanceBetween = (math.pow(point1[1],2) + math.pow(point2[1],2)) - ((2*point1[1]*point2[1])*(math.cos(degreesBetween)))
