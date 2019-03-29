7fb6e9e262500884e0ec448d62f6c0f4c3a8a150
122004 <ashtree012@gmail.com>
2019-03-28 20:13:52 -0500

    Finished Comments!
    All set with this portion of LiDAR! Up-to-date, helpful comments included. So excited to implement it!

--- Lidar/AutoHatch/distance_methods.py
+++ Lidar/AutoHatch/distance_methods.py
@@ -16,14 +16,14 @@ relaventPoints = []
 #PORT_NAME = '/dev/ttyUSB0'
 PORT_NAME = 'COM3'
 class auto_hatch:
-    a = distanceBetween #distance from point 1 to point 2 in the hatch slot area, Katherine will give.
-    dist = finalA #distance from LiDAR to point 2, Katherine will also give.
+    a = distanceBetween #distance from point 1 to point 2 in the hatch slot area
+    dist = finalA #distance from LiDAR to point 2
     ahalf = a/2 #finds the middle of point 1 and 2 in the hatch slot area
-    dega = finalArray[1][0] - finalArray[0][0] #Katherine will give us this value - angle mesured from LiDAR between point 2 and 3.
+    dega = finalArray[1][0] - finalArray[0][0] #angle mesured from LiDAR between point 2 and 3
     degb = [dist/(ahalf/math.degrees(math.sin(dega)))] #uses the law of signs to find angle b
     degc = 180-(dega+degb) #this finds the missing angle,or angle "c", by doing 180-(a+b) = c
     cargdist = [math.exp(ahalf,2) + math.exp(dist, 2)]-[2(ahalf)(dist)(math.degrees(math.cos(degc)))]
-    #uses the law of cosign to find the squared distance from LiDAR to ahalf.
+    #uses the law of cosine to find the squared distance from LiDAR to ahalf
     fincargdist = math.sqrt(cargdist)
     #ROOTED it to isolate the actual, final, totally accurate distance between LiDAR and ahalf. You are so welcome.
 
